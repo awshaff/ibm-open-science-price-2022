52ba98378687a148e467f5c34f36ac1db3441349
Abdurrahman Shaffar <abdurrahman.shaffar@khu.ac.kr>
2023-03-09 15:43:53 +0900

    Delete unnecessary files

--- HVQE/data/kagome/periodic/own_graph/opg/energy/4x2/run_VQE.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-#SBATCH -N 1
-#SBATCH -n 6
-#SBATCH -t 5-00:00:00
-#SBATCH --mem=150GB
-#SBATCH -p fat_soil_shared
-
-python3 ~/HVQE/HVQE.py $PWD 192 1
--- HVQE/data/kagome/periodic/own_graph/ops/energy/3x2/run_HVQE.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-#SBATCH -N 1
-#SBATCH -n 2
-#SBATCH -t 5-00:00:00
-#SBATCH -p gpu_shared
-#SBATCH --mem=1GB
-#SBATCH --gres=gpu:1
-
-python3 ~/HVQE/HVQE.py $PWD 276 3  
